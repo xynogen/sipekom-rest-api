353795f592b90d449e453a0e6716001555a535c4
xynogen <fikri3142857@gmail.com>
2023-07-15 00:34:09 +0700

    add placeholder remove image

--- handler/photoHandler.go
+++ handler/photoHandler.go
@@ -2,8 +2,6 @@ package handler
 
 import (
 	"os"
-	"sipekom-rest-api/model/response"
-	"sipekom-rest-api/model/static"
 	"sipekom-rest-api/utils"
 
 	"github.com/gofiber/fiber/v2"
@@ -15,23 +13,21 @@ import (
 // @Param photo_name path string true "Photo Name"
 // @Description get Photo from photo name
 // @Tags Photo
-// @Success 200 {object} response.Response
+// @Success 200
 // @Router /api/photo/{photo_name} [get]
 func GetPhoto(c *fiber.Ctx) error {
+	placeholder, err := os.ReadFile("data/photo/placeholder.png")
 	photoName := c.AllParams()["photo_name"]
-	resp := new(response.Response)
-	resp.Status = static.StatusError
-	resp.Data = nil
 
 	if !utils.IsExist("data/photo/" + photoName) {
-		resp.Message = "Photo not Exist"
-		return c.Status(fiber.StatusNotFound).JSON(resp)
+		c.Set("content-type", "image/png")
+		return c.Status(fiber.StatusOK).Send(placeholder)
 	}
 
 	photoBytes, err := os.ReadFile("data/photo/" + photoName)
 	if err != nil {
-		resp.Message = "Failed Load Photo"
-		return c.Status(fiber.StatusInternalServerError).JSON(resp)
+		c.Set("content-type", "image/png")
+		return c.Status(fiber.StatusOK).Send(placeholder)
 	}
 
 	c.Set("content-type", "image/jpg")
